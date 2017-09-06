2e9a162486db5ba21400df630e7aabb38b03cca4
John Lucey <contact@johnlucey.co.za>
2017-09-06 17:12:12 +0200

    add ability to take turns between X and Y moves

--- src/index.js
+++ src/index.js
@@ -15,14 +15,18 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     // We call .slice() to copy the squares array instead of mutating the existing array
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -33,7 +37,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
